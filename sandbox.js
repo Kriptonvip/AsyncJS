a8e72b61ffb3dab73dcd6e6fec5146192b3f2f31
Роман Романенко <simenk@mail.ru>
2022-01-03 15:06:33 +0300

    fetch working

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,10 @@
-const getTodos = (resource) => {
-    return new Promise ((resolve, reject)=> {
-        const request = new XMLHttpRequest();
+// Fetch api
 
-        request.addEventListener('readystatechange', () => {
-            if(request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText)
-                resolve(data);
-            } else if (request.readyState === 4) {
-                reject('Could not fetch the data');
-            }
-        });
-
-request.open('GET', resource);
-request.send();
-    })
-    
-}
-
-getTodos('json/luigi.json').then(data => {
-    console.log('resolve 1:',data);
-    return getTodos('json/mario.json');
+fetch('json/luigi.json').then(response => {
+console.log('resolve', response);
+return response.json();
 }).then(data => {
-    console.log('resolve 2:', data);
-    return getTodos('json/shaun.json');
-}).then(data => {
-    console.log('resolve 3:', data);
-}).catch(err => console.log(err));
-
+    console.log(data);
+}).catch (err => {
+    console.log('rejected', err);
+});
